55171e8f434c425c9899105d6e94afb88e8c196b
tomo <tomouenishi@gmail.com>
2016-06-10 07:27:12 +0000

    Make a basic User model (including secure passwords)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,19 +11,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help |#{@base_title}"
+    assert_select "title", "Help|Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About |#{@base_title}"
+    assert_select "title", "About|Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact|Ruby on Rails Tutorial Sample App"
   end
 
 end
